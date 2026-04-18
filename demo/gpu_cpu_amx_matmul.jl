e6b1104306413d573cb1a8e835697e84092b7e4f
PetarMarendic <petar.marendic@outlook.com>
2026-04-18 17:57:35 +0200

    Update gpu_cpu_amx_matmul.jl

--- demo/gpu_cpu_amx_matmul.jl
+++ demo/gpu_cpu_amx_matmul.jl
@@ -1,7 +1,8 @@
 # Demo that will stress each of the compute engines on Apple Silicon: GPU, CPU, AMX
-# This is the verbatim code shown in the video: https://youtu.be/HX1B0tlODvY
-# N = 16384 will place a significant load on DRAM and was chose to push the system towards high power usage. 
-# You may play with smaller values to find the optimal compute throughput of your machine
+# This is the verbatim code shown in my Youtube video: https://youtu.be/HX1B0tlODvY
+# This code was carefully crafted to be used for stressing the power consumption of each of the compute engines on Apple Silicon
+# N = 16384 will place a significant load on DRAM and was chosen to push the system towards high power usage. 
+# You may play with smaller N values to find the optimal compute throughput of your machine
 
 using LinearAlgebra, BenchmarkTools, Metal
 
